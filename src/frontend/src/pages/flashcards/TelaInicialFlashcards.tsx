144ede085861c1e2d89b9478b733a185100cfb45
Anna Beatriz Valderrama dos Santos <ra2357032>
2025-05-13 10:00:39 -0300

    flashcards

--- src/frontend/src/pages/flashcards/TelaInicialFlashcards.tsx
+++ src/frontend/src/pages/flashcards/TelaInicialFlashcards.tsx
@@ -1,58 +1,108 @@
-import React from 'react';
-import { IonPage, IonContent, IonIcon, IonButton } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonPage, IonContent, IonIcon, IonButton, IonPopover, IonGrid, IonRow, IonCol } from '@ionic/react';
 import Header from '../../components/Header';
-import { school } from 'ionicons/icons';
+import { alertCircle, school, close } from 'ionicons/icons';
 import './css/geralTelaInicial.css';
 import './css/uiTelaInicial.css';
 import './css/layoutsTelaInicial.css';
 import './css/pie.css';
 
 const TelaInicialFlashcards: React.FC = () => {
+  const [popoverVisible, setPopoverVisible] = useState<boolean>(false);
+  const [popoverEvent, setPopoverEvent] = useState<any>(undefined);
+
+  const handlePopoverClick = (event: React.MouseEvent) => {
+    setPopoverEvent(event.nativeEvent); 
+    setPopoverVisible(true);            
+  };
+
+  const closePopover = () => {
+    setPopoverVisible(false);
+  };
+
   return (
+
     <IonPage className="pagina">
       <Header />
+
       <IonContent>
-        <div id="bodyTelaInicialFlashcards">
-          <div id="revisao">
-            <div>
-            <IonIcon icon={school} id="iconeCapelo"/>
-            <div id="d1">
-                <p className="txtGeral">Revisão do dia!</p>
-                <IonButton className="revisaoGeral">
+        <IonGrid id="bodyTelaInicialFlashcards">
+          <IonRow id="revisao">
+            <IonCol id="dCapelo">
+              <IonIcon icon={school} id="iconeCapelo" />
+            </IonCol>
+            <IonCol id="d1">
+                  <p className="txtGeral">Revisão do dia!</p>
+                  <IonButton className="revisaoGeral">
                     <span className="textIniciar">Iniciar</span>
                     <span className="svg">
-                        <svg id="btnIniciar"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="50"
-                        height="20"
-                        fill="none"
-                        >
-                        <path
-                            fill="white"
-                            d="M10 7.519l-.939-.344h0l.939.344zm14.386-1.205l-.981-.192.981.192zm1.276 5.509l.537.843.148-.094.107-.139-.792-.611zm4.819-4.304l-.385-.923h0l.385.923zm7.227.707a1 1 0 0 0 0-1.414L31.343.448a1 1 0 0 0-1.414 0 1 1 0 0 0 0 1.414l5.657 5.657-5.657 5.657a1 1 0 0 0 1.414 1.414l6.364-6.364zM1 7.519l.554.833.029-.019.094-.061.361-.23 1.277-.77c1.054-.609 2.397-1.32 3.629-1.787.617-.234 1.17-.392 1.623-.455.477-.066.707-.008.788.034.025.013.031.021.039.034a.56.56 0 0 1 .058.235c.029.327-.047.906-.39 1.842l1.878.689c.383-1.044.571-1.949.505-2.705-.072-.815-.45-1.493-1.16-1.865-.627-.329-1.358-.332-1.993-.244-.659.092-1.367.305-2.056.566-1.381.523-2.833 1.297-3.921 1.925l-1.341.808-.385.245-.104.068-.028.018c-.011.007-.011.007.543.84zm8.061-.344c-.198.54-.328 1.038-.36 1.484-.032.441.024.94.325 1.364.319.45.786.64 1.21.697.403.054.824-.001 1.21-.09.775-.179 1.694-.566 2.633-1.014l3.023-1.554c2.115-1.122 4.107-2.168 5.476-2.524.329-.086.573-.117.742-.115s.195.038.161.014c-.15-.105.085-.139-.076.685l1.963.384c.192-.98.152-2.083-.74-2.707-.405-.283-.868-.37-1.28-.376s-.849.069-1.274.179c-1.65.43-3.888 1.621-5.909 2.693l-2.948 1.517c-.92.439-1.673.743-2.221.87-.276.064-.429.065-.492.057-.043-.006.066.003.155.127.07.099.024.131.038-.063.014-.187.078-.49.243-.94l-1.878-.689zm14.343-1.053c-.361 1.844-.474 3.185-.413 4.161.059.95.294 1.72.811 2.215.567.544 1.242.546 1.664.459a2.34 2.34 0 0 0 .502-.167l.15-.076.049-.028.018-.011c.013-.008.013-.008-.524-.852l-.536-.844.019-.012c-.038.018-.064.027-.084.032-.037.008.053-.013.125.056.021.02-.151-.135-.198-.895-.046-.734.034-1.887.38-3.652l-1.963-.384zm2.257 5.701l.791.611.024-.031.08-.101.311-.377 1.093-1.213c.922-.954 2.005-1.894 2.904-2.27l-.771-1.846c-1.31.547-2.637 1.758-3.572 2.725l-1.184 1.314-.341.414-.093.117-.025.032c-.01.013-.01.013.781.624zm5.204-3.381c.989-.413 1.791-.42 2.697-.307.871.108 2.083.385 3.437.385v-2c-1.197 0-2.041-.226-3.19-.369-1.114-.139-2.297-.146-3.715.447l.771 1.846z"
-                        ></path>
-                        </svg>
-                    </span>
-                </IonButton>
-            </div>
-            </div>
-            <div>
-                <svg viewBox="0 0 250 250" className="circular-progress">
-                    <circle className="bg"
-                        cx="125" cy="125" r="115" fill="none" stroke="#ddd" stroke-width="20"
-                    ></circle>
-                    <circle className="fg"
-                        cx="125" cy="125" r="115" fill="none" stroke="#5394fd" stroke-width="20"
-                        stroke-dasharray="361.25 361.25"
-                    ></circle>
-                </svg>
-            </div>
-          </div>
-          <div id="linhaDivisao"></div>
-          <div id="decks">
-
-          </div>
-        </div>
+                          <svg id="btnIniciar"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="50"
+                          height="20"
+                          fill="none"
+                          >
+                          <path
+                              fill="white"
+                              d="M10 7.519l-.939-.344h0l.939.344zm14.386-1.205l-.981-.192.981.192zm1.276 5.509l.537.843.148-.094.107-.139-.792-.611zm4.819-4.304l-.385-.923h0l.385.923zm7.227.707a1 1 0 0 0 0-1.414L31.343.448a1 1 0 0 0-1.414 0 1 1 0 0 0 0 1.414l5.657 5.657-5.657 5.657a1 1 0 0 0 1.414 1.414l6.364-6.364zM1 7.519l.554.833.029-.019.094-.061.361-.23 1.277-.77c1.054-.609 2.397-1.32 3.629-1.787.617-.234 1.17-.392 1.623-.455.477-.066.707-.008.788.034.025.013.031.021.039.034a.56.56 0 0 1 .058.235c.029.327-.047.906-.39 1.842l1.878.689c.383-1.044.571-1.949.505-2.705-.072-.815-.45-1.493-1.16-1.865-.627-.329-1.358-.332-1.993-.244-.659.092-1.367.305-2.056.566-1.381.523-2.833 1.297-3.921 1.925l-1.341.808-.385.245-.104.068-.028.018c-.011.007-.011.007.543.84zm8.061-.344c-.198.54-.328 1.038-.36 1.484-.032.441.024.94.325 1.364.319.45.786.64 1.21.697.403.054.824-.001 1.21-.09.775-.179 1.694-.566 2.633-1.014l3.023-1.554c2.115-1.122 4.107-2.168 5.476-2.524.329-.086.573-.117.742-.115s.195.038.161.014c-.15-.105.085-.139-.076.685l1.963.384c.192-.98.152-2.083-.74-2.707-.405-.283-.868-.37-1.28-.376s-.849.069-1.274.179c-1.65.43-3.888 1.621-5.909 2.693l-2.948 1.517c-.92.439-1.673.743-2.221.87-.276.064-.429.065-.492.057-.043-.006.066.003.155.127.07.099.024.131.038-.063.014-.187.078-.49.243-.94l-1.878-.689zm14.343-1.053c-.361 1.844-.474 3.185-.413 4.161.059.95.294 1.72.811 2.215.567.544 1.242.546 1.664.459a2.34 2.34 0 0 0 .502-.167l.15-.076.049-.028.018-.011c.013-.008.013-.008-.524-.852l-.536-.844.019-.012c-.038.018-.064.027-.084.032-.037.008.053-.013.125.056.021.02-.151-.135-.198-.895-.046-.734.034-1.887.38-3.652l-1.963-.384zm2.257 5.701l.791.611.024-.031.08-.101.311-.377 1.093-1.213c.922-.954 2.005-1.894 2.904-2.27l-.771-1.846c-1.31.547-2.637 1.758-3.572 2.725l-1.184 1.314-.341.414-.093.117-.025.032c-.01.013-.01.013.781.624zm5.204-3.381c.989-.413 1.791-.42 2.697-.307.871.108 2.083.385 3.437.385v-2c-1.197 0-2.041-.226-3.19-.369-1.114-.139-2.297-.146-3.715.447l.771 1.846z"
+                          ></path>
+                          </svg>
+                      </span>
+                  </IonButton>
+            </IonCol>
+            <IonCol id="d2">
+              <IonButton id="btnAlerta" onClick={handlePopoverClick}>
+                <IonIcon icon={alertCircle} id="iconeAlerta" />
+              </IonButton>
+              <IonPopover
+                isOpen={popoverVisible}
+                event={popoverEvent}
+                onDidDismiss={closePopover}
+                className="popoverGeral"
+              >
+                <div id="branco">
+                  <IonButton onClick={closePopover}>
+                    <IonIcon icon={close} id="iconeFechar" />
+                  </IonButton>        
+                  <p>O modo de revisão geral apresenta todos os flashcards do dia que devem ser revisados
+                     (indepente da matéria), para que você possa treinar todas as matérias!</p>          
+                </div>
+              </IonPopover>
+            </IonCol>
+            <IonCol className="colFlex">
+              <svg viewBox="0 0 250 250" className="circular-progress">
+                <circle
+                  className="bg"
+                  cx="125"
+                  cy="125"
+                  r="115"
+                  fill="none"
+                  stroke="#ddd"
+                  strokeWidth="20"
+                />
+                <circle
+                  className="fg"
+                  cx="125"
+                  cy="125"
+                  r="115"
+                  fill="none"
+                  stroke="#5394fd"
+                  strokeWidth="20"
+                  strokeDasharray="361.25 361.25"
+                />
+              </svg>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <div id="linhaDivisao"></div>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
