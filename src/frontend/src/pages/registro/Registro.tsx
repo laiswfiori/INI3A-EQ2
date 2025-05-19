a662073fd16735b2f8bb4771d36e44ecc881ddb3
Ana Luiza Aguiar de Oliveira <ra2357005>
2025-05-19 12:16:57 -0300

    Responsividade Cadastro

--- src/frontend/src/pages/registro/Registro.tsx
+++ src/frontend/src/pages/registro/Registro.tsx
@@ -53,29 +53,33 @@ const Registro: React.FC<RegistroProps> = ({ goToLogin }) => {
             <div className="div1">
               <h1><b>Cadastre-se em nossa plataforma!</b></h1><br />
               <IonButton className="btnGoogle"><b>Continue com o Google</b></IonButton><br />
-              <h3>ou:</h3><br />
-              <p><b>Nome</b></p>
+              <div id="ou">
+                  <div className="linhas"><p className="txtLinha">la</p></div>
+                  <h3 id="txtOu">Ou</h3>
+                  <div className="linhas"></div>
+              </div>
+              <p id="label1"><b>Nome</b></p>
               <input 
                 type="text"
                 value={name}
                 onChange={(e) => setNome(e.target.value)}
                 /><br /><br />
-              <p><b>Email</b></p>
+              <p id="label1"><b>Email</b></p>
               <input
                 type="email" 
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 /><br /><br />
-              <p><b>Senha</b></p>
+              <p id="label1"><b>Senha</b></p>
               <input
                 type="password"
                 value={password}
                 onChange={(e) => setSenha(e.target.value)}
                 /><br /><br />
               <IonButton className="btnCadastrar" onClick={handleCadastrar}><b>Cadastrar</b></IonButton><br />
-              <h4>Já possui uma conta?
+              <h3 id="direita">Já possui uma conta?
               <IonButton className="btnLogin" onClick={goToLogin}>Faça login</IonButton>
-              </h4>
+              </h3>
             </div>
 
             <div className="div2">
