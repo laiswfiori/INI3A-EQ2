1e6784876f7b04f47b390cee5d23f9875d3ff3b6
Pedro Alexandre Pimenta <p.pimenta@unesp.br>
2025-08-18 23:15:49 -0300

    Corrige a estrutura de diretórios do projeto

--- INI3A-EQ2/src/frontend/src/utils/formatters.ts
+++
@@ -1,9 +0,0 @@
-
-export function formatToH_i(time: string): string {
-  if (!time) return '';
-  const [hourStr, minuteStr] = time.split(':');
-  if (!hourStr || !minuteStr) return '';
-  
-  const hour = parseInt(hourStr, 10);
-  return `${hour}:${minuteStr}`;
-}
